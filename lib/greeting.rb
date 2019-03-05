fe6eda96a5a93fa457d174a6d715c5941924911b
Joe Tustin <j.tustin@gmail.com>
2019-03-05 20:44:56 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,7 @@
 # code the #greeting method here!
 # I added name = name.strip inside this function for the first part of the test to get them to pass.
-# I know it should have gone on the bin/greet
+# I know it should have gone on the bin/greet.  
+# If I remove it it fails one of the original tests 
 
 
 def greeting(name)
