a3d9686b4f4ea7ac797d2166928f027e69f377d5
Joe Tustin <j.tustin@gmail.com>
2019-03-05 20:22:52 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
 
 def greeting(name)
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+  puts "Hello #{name}. It's nice to meet you."
 end
